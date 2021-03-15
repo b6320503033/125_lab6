a5fb922ea9a3fc699f9a3c93b171e89209d98b3c
b6320503033 <thida.sa@ku.th>
2021-03-15 19:42:19 +0700

    Update lab6_1.c

--- lab6_1.c
+++ lab6_1.c
@@ -2,7 +2,7 @@
 int main()
 {
     int n,i,j;
-    char c;
+    char c,h,k;
     scanf("%d",&n);
     if(n>=1&&n<=10000);
     fflush(stdin);
@@ -16,7 +16,7 @@ int main()
                 break;
         }
 
-    for(c='A'; c<='Z'; c++)
+    for(c='A',h='a'; c<='Z',h<='z'; c++,h++)
     {
         for(i=0; i<n; i++)
             if(a[i][0]==c)
@@ -29,6 +29,17 @@ int main()
 
                 }
             }
+            else if(a[i][0]==h)
+            {
+                for(j=0; j<1000; j++)
+                {
+                    printf("%c",a[i][j]);
+                    if(a[i][j]=='\n')
+                        break;
+
+                }
+            }
+
     }
 
    return 0;
